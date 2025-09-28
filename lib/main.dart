9bbbf2502acb1899581014d0bd0c217344eafefe
Clerin Balakrishnan <sheronclerin1805@gmail.com>
2025-09-28 12:17:52 +0530

    connect the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'signin.dart';
 import 'patientDashboard.dart';
 import 'patientDashboardLog.dart';
 import 'patientProfile.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const SignInApp());
 }
